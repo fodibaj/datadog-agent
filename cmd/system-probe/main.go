01efd873cdade776c5368fc7aac9d6f6223d19e5
Paul Cacheux <paul.cacheux@datadoghq.com>
2023-11-28 15:20:26 +0100

    allow building system probe

--- cmd/system-probe/main.go
+++ cmd/system-probe/main.go
@@ -3,7 +3,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016-present Datadog, Inc.
 
-//go:build linux
+//go:build linux || darwin
 
 // Package main is the entrypoint for system-probe process
 package main
