d20259cf48817004f92666c4b3c52e9070a19ea9
Maxime mouial <maxime.mouial@gmail.com>
2024-01-29 18:04:53 +0100

    Fetch process-agent metadata from the process-agent process

--- comp/metadata/inventoryagent/inventoryagentimpl/inventoryagent_test.go
+++ comp/metadata/inventoryagent/inventoryagentimpl/inventoryagent_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/DataDog/datadog-agent/comp/core/sysprobeconfig/sysprobeconfigimpl"
 	pkgconfig "github.com/DataDog/datadog-agent/pkg/config"
 	configFetcher "github.com/DataDog/datadog-agent/pkg/config/fetcher"
+	"github.com/DataDog/datadog-agent/pkg/config/model"
 	pkgconfigmodel "github.com/DataDog/datadog-agent/pkg/config/model"
 	"github.com/DataDog/datadog-agent/pkg/serializer"
 	"github.com/DataDog/datadog-agent/pkg/util/flavor"
@@ -307,7 +308,7 @@ func TestStatusHeaderProvider(t *testing.T) {
 
 func TestFetchSecurityAgent(t *testing.T) {
 	defer func() {
-		fetchSecurityConfig = configFetcher.FetchSecurityAgentConfig
+		fetchSecurityConfig = configFetcher.SecurityAgentConfig
 	}()
 	fetchSecurityConfig = func(config pkgconfigmodel.Reader) (string, error) {
 		return "", fmt.Errorf("some error")
@@ -331,4 +332,40 @@ func TestFetchSecurityAgent(t *testing.T) {
 
 	assert.True(t, ia.data["feature_cspm_enabled"].(bool))
 	assert.True(t, ia.data["feature_cspm_host_benchmarks_enabled"].(bool))
+}
+
+func TestFetchProcessAgent(t *testing.T) {
+	defer func(original func(cfg model.Reader) (string, error)) {
+		fetchProcessConfig = original
+	}(fetchProcessConfig)
+
+	fetchProcessConfig = func(config pkgconfigmodel.Reader) (string, error) {
+		return "", fmt.Errorf("some error")
+	}
+
+	ia := getTestInventoryPayload(t, nil, nil)
+	ia.fetchProcessAgentMetadata()
+
+	assert.False(t, ia.data["feature_process_enabled"].(bool))
+	assert.False(t, ia.data["feature_process_language_detection_enabled"].(bool))
+	// default to true in the process agent configuration
+	assert.True(t, ia.data["feature_processes_container_enabled"].(bool))
+
+	fetchProcessConfig = func(config pkgconfigmodel.Reader) (string, error) {
+		return `
+process_config:
+  process_collection:
+    enabled: true
+  container_collection:
+    enabled: true
+language_detection:
+  enabled: true
+`, nil
+	}
+
+	ia.fetchProcessAgentMetadata()
+
+	assert.True(t, ia.data["feature_process_enabled"].(bool))
+	assert.True(t, ia.data["feature_processes_container_enabled"].(bool))
+	assert.True(t, ia.data["feature_process_language_detection_enabled"].(bool))
 }
